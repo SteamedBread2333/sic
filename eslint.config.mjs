85126643c4c1d3ad8cef3a59d464e7fdde6770e3
pipi <rzh881004@icloud.com>
2025-03-07 15:59:24 +0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+	{ files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+	{ languageOptions: { globals: globals.node } },
 ];
